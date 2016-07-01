281e47933710d730dab9a0d0ad79fbbbc108c5f8
d@ni€| <artchounindaniel@gmail.com>
2016-07-01 13:05:53 +0200

    Added 'insert.sql' and updated conceptual data model.

---
+++ WebContent/WEB-INF/database_conception/insert.sql
@@ -0,0 +1,12 @@
+INSERT INTO Language(language) 
+VALUES 
+('chinois mandarin'),
+('anglais'),
+('espagnol'),
+('hindi'),
+('français'),
+('russe'),
+('malais-indonésien'),
+('arabe'),
+('portuguais'),
+('bengali');
